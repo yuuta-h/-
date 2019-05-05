a42f0bc97c1a472c51f19e26ae15409a1afaa7f2
Local kanno tatsuki <gp11ckanno08@gmail.com>
2019-05-05 23:05:22 +0900

    メモリリーク特定機能

--- GameProtType.ver.0.00/main.h
+++ GameProtType.ver.0.00/main.h
@@ -22,6 +22,14 @@
 //*****************************************************************************
 // マクロ定義
 //*****************************************************************************
+#ifdef _DEBUG
+#define   new                   new(_NORMAL_BLOCK, __FILE__, __LINE__)
+#define   malloc(s)             _malloc_dbg(s, _NORMAL_BLOCK, __FILE__, __LINE__)
+#define   calloc(c, s)          _calloc_dbg(c, s, _NORMAL_BLOCK, __FILE__, __LINE__)
+#define   realloc(p, s)         _realloc_dbg(p, s, _NORMAL_BLOCK, __FILE__, __LINE__)
+#define   _recalloc(p, c, s)    _recalloc_dbg(p, c, s, _NORMAL_BLOCK, __FILE__, __LINE__)
+#define   _expand(p, s)         _expand_dbg(p, s, _NORMAL_BLOCK, __FILE__, __LINE__)
+#endif
 // ２Ｄポリゴン頂点フォーマット( 頂点座標[2D] / 反射光 / テクスチャ座標 )
 #define	FVF_VERTEX_2D	(D3DFVF_XYZRHW | D3DFVF_DIFFUSE | D3DFVF_TEX1)
 #define	NUM_VERTEX		(4)		// 頂点数
